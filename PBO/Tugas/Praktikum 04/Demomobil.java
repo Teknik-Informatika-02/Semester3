66e9bf14f0c23efacff365d8f94e9c61a24b0b47
Ghilman <ghilman6@gmail.com>
2019-10-11 20:42:31 +0700

    Tugas praktikum 2

---
+++ PBO/Tugas/Praktikum 04/Demomobil.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Demomobil{
+    public static void main(String[] args) {
+        mobil sedan = new mobil("SUV","BMW",1999);
+        /*
+        sedan.inMerek("BMW");
+        sedan.inJenis("SUV");
+        sedan.inCC(1999);
+        */
+
+        //Scanner input = new Scanner(System.in);
+
+        System.out.println("Sedan");
+        System.out.println("Jenis : "+ sedan.typeVehicle());
+        System.out.println("Merek : "+ sedan.MerekVehicle());
+        System.out.println("CC : "+ sedan.ccVehicle());
+
+        sedan.tampil();
+    }
+}
---
+++ PBO/Tugas/Praktikum 04/mobil.java
@@ -0,0 +1,41 @@
+public class mobil{
+    private String jenis;
+    private String merek;
+    private int cc;
+
+    public mobil(String jenis, String merek, int cc){
+        this.jenis=jenis;
+        this.merek = merek;
+        this.cc = cc;
+    }
+
+    public void tampil(){
+        System.out.println("jenis ="+this.jenis);
+        System.out.println("merek ="+this.merek);
+        System.out.println("cc ="+this.cc);
+    }
+
+    public void inJenis(String jenis) {
+        this.jenis = jenis;
+    }
+    
+    public void inMerek(String merek) {
+        this.merek = merek;
+    }
+
+    public void inCC(int CC) {
+        this.cc = CC;
+    }
+
+    public String typeVehicle() {
+        return jenis;
+    }
+
+    public String MerekVehicle() {
+        return merek;
+    }
+    
+    public int ccVehicle() {
+        return cc;
+    }
+}
